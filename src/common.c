9c78164290922562c8d6a8d751662b4d0168b834
Dengfeng Liu <liudf0716@qq.com>
2017-09-29 16:11:01 +0800

    Update common.c

--- src/common.c
+++ src/common.c
@@ -567,3 +567,18 @@ int hexToInt(char s[], int len)
 
 	return value;
 }
+
+void trim(char *str)
+{
+    int i;
+    int begin = 0;
+    int end = strlen(str) - 1;
+
+    while (isspace((int)str[begin])) begin++;
+    while ((end >= begin) && isspace((int)str[end])) end--;
+    // Shift all characters back to the start of the string array.
+    for (i = begin; i <= end; i++)
+        str[i - begin] = str[i];
+    str[i - begin] = '\0'; // Null terminate string.
+}
+
