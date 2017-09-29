aa39501efe22765aacd8e49f3b2b64d943bb738a
Dengfeng Liu <liudf0716@qq.com>
2017-09-29 19:03:07 +0800

    Update adu-restful-serv.c

--- src/adu-restful-serv.c
+++ src/adu-restful-serv.c
@@ -13,7 +13,7 @@ extern struct globals G;
 
 static void send_error_result(struct mg_connection *nc, const char *msg) {
 	/* Send headers */
-  	mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nTransfer-Encoding: chunked\r\n\r\n");
+  	mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
   	mg_printf_http_chunk(nc, "Error: %s", msg);
   	mg_send_http_chunk(nc, "", 0); /* Send empty chunk, the end of response */
 }
