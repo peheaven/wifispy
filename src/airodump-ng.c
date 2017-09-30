4a7aa355c84a2c7c5efd9d0a24e0a66958e1ed0f
Dengfeng Liu <liudf0716@qq.com>
2017-09-30 12:03:31 +0800

    Update airodump-ng.c

--- src/airodump-ng.c
+++ src/airodump-ng.c
@@ -76,6 +76,7 @@
 #include "common.h"
 #include "adu-restful-serv.h"
 #include "daemon.h"
+#include "aircrack-ng-util.h"
 
 struct globals G;
 
@@ -138,99 +139,6 @@ extern int is_string_number(const char * str);
 
 void dump_sort( void );
 void dump_print( int ws_row, int ws_col, int if_num );
-
-char * get_manufacturer_from_string(char * buffer) {
-	char * manuf = NULL;
-	char * buffer_manuf;
-	if (buffer != NULL && strlen(buffer) > 0) {
-		buffer_manuf = strstr(buffer, "(hex)");
-		if (buffer_manuf != NULL) {
-			buffer_manuf += 6; // skip '(hex)' and one more character (there's at least one 'space' character after that string)
-			while (*buffer_manuf == '\t' || *buffer_manuf == ' ') {
-				++buffer_manuf;
-			}
-
-			// Did we stop at the manufacturer
-			if (*buffer_manuf != '\0') {
-
-				// First make sure there's no end of line
-				if (buffer_manuf[strlen(buffer_manuf) - 1] == '\n' || buffer_manuf[strlen(buffer_manuf) - 1] == '\r') {
-					buffer_manuf[strlen(buffer_manuf) - 1] = '\0';
-					if (*buffer_manuf != '\0' && (buffer_manuf[strlen(buffer_manuf) - 1] == '\n' || buffer[strlen(buffer_manuf) - 1] == '\r')) {
-						buffer_manuf[strlen(buffer_manuf) - 1] = '\0';
-					}
-				}
-				if (*buffer_manuf != '\0') {
-					if ((manuf = (char *)malloc((strlen(buffer_manuf) + 1) * sizeof(char))) == NULL) {
-						perror("malloc failed");
-						return NULL;
-					}
-					snprintf(manuf, strlen(buffer_manuf) + 1, "%s", buffer_manuf);
-				}
-			}
-		}
-	}
-
-	return manuf;
-}
-
-void textcolor(int attr, int fg, int bg)
-{	char command[13];
-
-	/* Command is the control command to the terminal */
-	snprintf(command, sizeof(command), "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
-	fprintf(stderr, "%s", command);
-	fflush(stderr);
-}
-
-void textcolor_fg(int fg)
-{	char command[13];
-
-	/* Command is the control command to the terminal */
-	snprintf(command, sizeof(command), "\033[%dm", fg + 30);
-	fprintf(stderr, "%s", command);
-	fflush(stderr);
-}
-
-void textcolor_bg(int bg)
-{	char command[13];
-
-	/* Command is the control command to the terminal */
-	snprintf(command, sizeof(command), "\033[%dm", bg + 40);
-	fprintf(stderr, "%s", command);
-	fflush(stderr);
-}
-
-void textstyle(int attr)
-{	char command[13];
-
-	/* Command is the control command to the terminal */
-	snprintf(command, sizeof(command), "\033[%im", attr);
-	fprintf(stderr, "%s", command);
-	fflush(stderr);
-}
-
-void reset_term() {
-  struct termios oldt,
-                 newt;
-  tcgetattr( STDIN_FILENO, &oldt );
-  newt = oldt;
-  newt.c_lflag |= ( ICANON | ECHO );
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt );
-}
-
-int mygetch( ) {
-  struct termios oldt,
-                 newt;
-  int            ch;
-  tcgetattr( STDIN_FILENO, &oldt );
-  newt = oldt;
-  newt.c_lflag &= ~( ICANON | ECHO );
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt );
-  ch = getchar();
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
-  return ch;
-}
 
 void resetSelection()
 {
@@ -735,53 +643,7 @@ char usage[] =
 "      --help                : Displays this usage screen\n"
 "\n";
 
-int is_filtered_netmask(unsigned char *bssid)
-{
-    unsigned char mac1[6] = {0};
-    unsigned char mac2[6] = {0};
-    int i;
 
-    for(i=0; i<6; i++)
-    {
-        mac1[i] = bssid[i]     & G.f_netmask[i];
-        mac2[i] = G.f_bssid[i] & G.f_netmask[i];
-    }
-
-    if( memcmp(mac1, mac2, 6) != 0 )
-    {
-        return( 1 );
-    }
-
-    return 0;
-}
-
-int is_filtered_essid(unsigned char *essid)
-{
-    int ret = 0;
-    int i;
-
-    if(G.f_essid)
-    {
-        for(i=0; i<G.f_essid_count; i++)
-        {
-            if(strncmp((char*)essid, G.f_essid[i], MAX_IE_ELEMENT_SIZE) == 0)
-            {
-                return 0;
-            }
-        }
-
-        ret = 1;
-    }
-
-#ifdef HAVE_PCRE
-    if(G.f_essid_regex)
-    {
-        return pcre_exec(G.f_essid_regex, NULL, (char*)essid, strnlen((char *)essid, MAX_IE_ELEMENT_SIZE), 0, 0, NULL, 0) < 0;
-    }
-#endif
-
-    return ret;
-}
 
 void update_rx_quality( )
 {
