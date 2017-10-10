4e9ff759e7db56d7b5e96d6f8640511fec6d7806
Dengfeng Liu <liudf0716@qq.com>
2017-10-10 14:31:09 +0800

    Update airodump-ng.c

--- src/airodump-ng.c
+++ src/airodump-ng.c
@@ -1972,29 +1972,6 @@ char * getBatteryString(void)
 
     return ret;
 }
-
-#define TSTP_SEC 1000000ULL /* It's a 1 MHz clock, so a million ticks per second! */
-#define TSTP_MIN (TSTP_SEC * 60ULL)
-#define TSTP_HOUR (TSTP_MIN * 60ULL)
-#define TSTP_DAY (TSTP_HOUR * 24ULL)
-
-static char *parse_timestamp(unsigned long long timestamp) {
-	static char s[15];
-	unsigned long long rem;
-	unsigned int days, hours, mins, secs;
-
-	days = timestamp / TSTP_DAY;
-	rem = timestamp % TSTP_DAY;
-	hours = rem / TSTP_HOUR;
-	rem %= TSTP_HOUR;
-	mins = rem / TSTP_MIN;
-	rem %= TSTP_MIN;
-	secs = rem / TSTP_SEC;
-
-	snprintf(s, 14, "%3ud %02u:%02u:%02u", days, hours, mins, secs);
-
-	return s;
-}
 					
 char * format_text_for_csv( const unsigned char * input, int len)
 {
